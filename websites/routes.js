ecd9d8094dc8be302817a4cbb807d840c83ce696
John Häggerud <john.haggerud@lnu.se>
2015-11-01 11:11:54 +0100

    added robots.txt fix spelling

--- websites/routes.js
+++ websites/routes.js
@@ -8,11 +8,10 @@ var debug = require("debug")("routes");
 
 module.exports = function(app) {
 
-    debug(__dirname + '/calendar/');
+    debug(__dirname + "/calendar/");
 
     // set routes for calendar - This is just static content
     app.use("/", express.static(__dirname + "/index/"));
-
 
     // set routes for calendar - This is just static content
     app.use("/calendar", express.static(__dirname + "/calendar/"));
@@ -20,8 +19,12 @@ module.exports = function(app) {
     // set routes for calendar - This is just static content
     app.use("/dinner", express.static(__dirname + "/restaurang/static"));
 
+    app.use("/dinner/robots.txt",  express.static(__dirname + "/restaurang/robots.txt"));
+
     // set routes for cinema - the specifik routes for this is in /cinema
     app.use("/cinema", require("./cinema"));
+
+    app.use("/cinema/robots.txt",  express.static(__dirname + "/cinema/robots.txt"));
 
     // set routes for calendar - This is just static content
     app.use("/cinema/static", express.static(__dirname + "/cinema/static/"));
